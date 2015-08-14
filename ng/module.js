2d08bd19b6da6002c18649fa37e8d9edb3170752
yusukehimeno <yuunyawach@gmail.com>
2015-08-14 14:47:57 +0900

    ActionSheetの実装

--- ng/module.js
+++ ng/module.js
@@ -1,118 +1,53 @@
-angular.module('todo', ['ionic'])
-/**
- * The Projects factory handles saving and loading projects
- * from local storage, and also lets us save and load the
- * last active project index.
- */
-.factory('Projects', function() {
-  return {
-    all: function() {
-      var projectString = window.localStorage['projects'];
-      if(projectString) {
-        return angular.fromJson(projectString);
-      }
-      return [];
-    },
-    save: function(projects) {
-      window.localStorage['projects'] = angular.toJson(projects);
-    },
-    newProject: function(projectTitle) {
-      // Add a new project
-      return {
-        title: projectTitle,
-        tasks: []
-      };
-    },
-    getLastActiveIndex: function() {
-      return parseInt(window.localStorage['lastActiveProject']) || 0;
-    },
-    setLastActiveIndex: function(index) {
-      window.localStorage['lastActiveProject'] = index;
-    }
-  }
-})
+var gvApp = angular.module('geographicVideo', ['ionic']);
 
-.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+/*
+gvApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('main', {url: "/main", abstract: true, templateUrl: "main.html"})
+    .state('main.top', {url: '/top', views: {'menuContent': {templateUrl: 'top.html', controller: 'topCtrl'} }  })
+    .state('main.navi', {url: '/navi', views: {'menuContent': {templateUrl: 'naviView.html', controller: 'nanviCtrl'} }  })
+    .state('main.video', {url: '/video', views: {'menuContent': {templateUrl: 'videoView.html', controller: 'videoCtrl'} }  });
 
-  // A utility function for creating a new project
-  // with the given projectTitle
-  var createProject = function(projectTitle) {
-    var newProject = Projects.newProject(projectTitle);
-    $scope.projects.push(newProject);
-    Projects.save($scope.projects);
-    $scope.selectProject(newProject, $scope.projects.length-1);
-  }
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/main/top');
+}]);
+*/
+
+gvApp.controller('mainCtrl', function($scope, $ionicActionSheet, $timeout){
+
+  $scope.items = [
+    {title: 'タイトルその1'},
+    {title: 'タイトルその2'},
+    {title: 'タイトルその3'},
+    {title: 'タイトルその4'},
+    {title: 'タイトルその5'},
+    {title: 'タイトルその6'}
+  ];
 
 
-  // Load or initialize projects
-  $scope.projects = Projects.all();
+  // Triggered on a button click, or some other target
+  $scope.show = function(index) {
 
-  // Grab the last active, or the first project
-  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+       { text: '探しに行く！' }
+     ],
+     titleText: $scope.items[index].title,
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+     },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
 
-  // Called to create a new project
-  $scope.newProject = function() {
-    var projectTitle = prompt('Project name');
-    if(projectTitle) {
-      createProject(projectTitle);
-    }
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
   };
-
-  // Called to select the given project
-  $scope.selectProject = function(project, index) {
-    $scope.activeProject = project;
-    Projects.setLastActiveIndex(index);
-    $ionicSideMenuDelegate.toggleLeft(false);
-  };
-
-  // Create our modal
-  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-    $scope.taskModal = modal;
-  }, {
-    scope: $scope
-  });
-
-  $scope.createTask = function(task) {
-    if(!$scope.activeProject || !task) {
-      return;
-    }
-    $scope.activeProject.tasks.push({
-      title: task.title
-    });
-    $scope.taskModal.hide();
-
-    // Inefficient, but save all the projects
-    Projects.save($scope.projects);
-
-    task.title = "";
-  };
-
-  $scope.newTask = function() {
-    $scope.taskModal.show();
-  };
-
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  }
-
-  $scope.toggleProjects = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-
-  // Try to create the first project, make sure to defer
-  // this by using $timeout so everything is initialized
-  // properly
-  $timeout(function() {
-    if($scope.projects.length == 0) {
-      while(true) {
-        var projectTitle = prompt('Your first project title:');
-        if(projectTitle) {
-          createProject(projectTitle);
-          break;
-        }
-      }
-    }
-  });
 
 });
