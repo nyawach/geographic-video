49c85747d206612871998b257ee1a2d6a86e69e5
yusukehimeno <yuunyawach@gmail.com>
2015-08-14 17:56:13 +0900

    ポップアップに変更

--- ng/module.js
+++ ng/module.js
@@ -1,19 +1,91 @@
 var gvApp = angular.module('geographicVideo', ['ionic']);
 
-/*
-gvApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+gvApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('main', {url: "/main", abstract: true, templateUrl: "main.html"})
-    .state('main.top', {url: '/top', views: {'menuContent': {templateUrl: 'top.html', controller: 'topCtrl'} }  })
-    .state('main.navi', {url: '/navi', views: {'menuContent': {templateUrl: 'naviView.html', controller: 'nanviCtrl'} }  })
-    .state('main.video', {url: '/video', views: {'menuContent': {templateUrl: 'videoView.html', controller: 'videoCtrl'} }  });
+    .state('home', {
+      url: '/',
+      templateUrl: 'index.html'
+    })
+    .state('navi', {
+      url: '/navi',
+      templateUrl: 'navi.html'
+    });
+    // .state('main.video', {
+    //   url: '/video',
+    //   views: {
+    //     'menuContent': {
+    //       templateUrl: 'videoView.html',
+    //       controller: 'videoCtrl'
+    //     }
+    //   }
+    // });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/main/top');
-}]);
-*/
+  $urlRouterProvider.otherwise('/');
+});
 
-gvApp.controller('mainCtrl', function($scope, $ionicActionSheet, $timeout){
+
+gvApp.controller('naviCtrl', function($scope, $ionicPopup, $ionicLoading, $compile){
+
+  function initialize() {
+    var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+    
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+    
+    //Marker + infowindow + angularjs compiled ng-click
+    var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+    var compiled = $compile(contentString)($scope);
+
+    var infowindow = new google.maps.InfoWindow({
+      content: compiled[0]
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Uluru (Ayers Rock)'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
+    $scope.map = map;
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  $scope.centerOnMe = function() {
+    if(!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function(error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+
+  $scope.clickTest = function() {
+    alert('Example of infowindow with ng-click')
+  };
+
+});
+
+gvApp.controller('mainCtrl', function($scope, $ionicPopup, $timeout){
 
   $scope.items = [
     {title: 'タイトルその1'},
@@ -25,29 +97,26 @@ gvApp.controller('mainCtrl', function($scope, $ionicActionSheet, $timeout){
   ];
 
 
-  // Triggered on a button click, or some other target
-  $scope.show = function(index) {
+  // A confirm dialog
+  $scope.showConfirm = function(index) {
 
-   // Show the action sheet
-   var hideSheet = $ionicActionSheet.show({
-     buttons: [
-       { text: '探しに行く！' }
-     ],
-     titleText: $scope.items[index].title,
-     cancelText: 'Cancel',
-     cancel: function() {
-          // add cancel code..
-     },
-     buttonClicked: function(index) {
-       return true;
-     }
-   });
+    var confirmPopup = $ionicPopup.confirm({
+      title: $scope.items[index].title,
+      template: 'この場所へ行ってみますか？'
+    });
 
-   // For example's sake, hide the sheet after two seconds
-   $timeout(function() {
-     hideSheet();
-   }, 2000);
+    confirmPopup.then(function(res) {
+      if(res) {
+        location.href = '/#/navi';
+        $scope.activeLocationIndex = index;
+      } else {
+        console.log('You are not sure');
+      }
+      
+    
+    });
 
   };
+
 
 });
